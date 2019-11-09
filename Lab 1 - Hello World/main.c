edfb415da632a613bdf9cb0a95359082da62a7b5
klynkin_kirill <klynkin.kirill@yandex.ru>
2019-11-09 17:19:04 +0300

    ,

--- Lab 1 - Hello World/main.c
+++ Lab 1 - Hello World/main.c
@@ -3,85 +3,81 @@ int main(void)
 
 
 {
-	int16_t mas[12];
-	mas[0]=-123;
-	mas[1]=-0x02;
-	mas[0]=0x01;
-	mas[11]=0xDDDD;
-//	RCC->APB2ENR|=RCC_APB2ENR_IOPCEN;
-//	RCC->APB2ENR|=RCC_APB2ENR_IOPAEN;
-//	GPIOA->CRL &= 0xF0000000;
-//	GPIOA->CRL |= 0x03333333;
-//	uint8_t counter=0;
-//	uint8_t flag=0;
+
+	RCC->APB2ENR|=RCC_APB2ENR_IOPCEN;
+	RCC->APB2ENR|=RCC_APB2ENR_IOPAEN;
+	GPIOA->CRL &= 0xF0000000;
+	GPIOA->CRL |= 0x03333333;
+	uint8_t counter=0;
+	uint8_t flag=0;
 	
 	while(1)
 	{
-//		if((GPIOC->IDR & GPIO_IDR_IDR1) != GPIO_IDR_IDR1) 
-//			{
-//    delay(1000000);
-//				if((GPIOC->IDR & GPIO_IDR_IDR1) != GPIO_IDR_IDR1) 
-//					{
-//				
-//					GPIOA->ODR &= 0xFF80;
-//					if(flag==0)
-//						{
-//							counter++;
-//								if(counter>9)
-//								flag=1;
-//						}
-//						else
-//						{
-//							counter --;
-//							if(counter==1)
-//							flag=0;
-//						}
-//						
-//						switch (counter)
-//						{
-//							case 1:
-//							GPIOA->ODR |=0x003F;
-//							break;
-//							
-//							case 2:
-//							GPIOA->ODR |=0x0006;
-//							break;
-//							
-//							case 3:
-//							GPIOA->ODR |=0x005B;
-//							break;
-//							
-//							case 4:
-//							GPIOA->ODR |=0x004F;
-//							break;
-//							
-//							case 5:
-//							GPIOA->ODR |=0x0066;
-//							break;
-//							
-//							case 6:
-//							GPIOA->ODR |=0x006D;
-//							break;
-//							
-//							case 7:
-//							GPIOA->ODR |=0x007D;
-//							break;
-//							
-//							case 8:
-//							GPIOA->ODR |=0x0007;
-//							break;
-//							
-//							case 9:
-//							GPIOA->ODR |=0x007F;
-//							break;
-//							
-//							case 10:
-//							GPIOA->ODR |=0x006F;
-//							break;
-//							
-//						}
-//					}
-//				}	
+		if((GPIOC->IDR & GPIO_IDR_IDR1) != GPIO_IDR_IDR1) 
+			{
+    delay(1000000);
+				if((GPIOC->IDR & GPIO_IDR_IDR1) != GPIO_IDR_IDR1) 
+					{
+				
+					GPIOA->ODR &= 0xFF80;
+					if(flag==0)
+						{
+							counter++;
+								if(counter>9)
+								flag=1;
+						}
+						else
+						{
+							counter --;
+							if(counter==1)
+							flag=0;
+						}
+						
+						switch (counter)
+						{
+							case 1:
+							GPIOA->ODR |=0x003F;
+							break;
+							
+							case 2:
+							GPIOA->ODR |=0x0006;
+							break;
+							
+							case 3:
+							GPIOA->ODR |=0x005B;
+							break;
+							
+							case 4:
+							GPIOA->ODR |=0x004F;
+							break;
+							
+							case 5:
+							GPIOA->ODR |=0x0066;
+							break;
+							
+							case 6:
+							GPIOA->ODR |=0x006D;
+							break;
+							
+							case 7:
+							GPIOA->ODR |=0x007D;
+							break;
+							
+							case 8:
+							GPIOA->ODR |=0x0007;
+							break;
+							
+							case 9:
+							GPIOA->ODR |=0x007F;
+							break;
+							
+							case 10:
+							GPIOA->ODR |=0x006F;
+							break;
+							
+						}
+					}
+				}	
 	}
 }
 
